8c423ee73128c0eef43352f39bcb1b426ef98f96
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-05-04 16:02:39 -0300

    Tabela com informacoes do produto

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$.getJSON('http://192.168.1.109:8080/list', function(data){
 		var list='';
+		var list='<option value="#"> Selecione uma opção. </option>';
 		for (var x=0; x<data.length;x++){
 			list+='<option value='+data[x].chave+'>' +data[x].nome+ '</option>';
 		}
@@ -12,10 +13,11 @@ function buscarproduto(){
 	$.getJSON('http://192.168.1.109:8080/list', function(data){
 		var i=$('#produtos').val();
 		var result='';
-		result+='Nome: ' + data[i-1].nome + '<br />';
-		result+='Valor: ' + data[i-1].valor + '<br />';
-		result+='Status: ' + data[i-1].status + '<br />';
-		result+='Estoque: ' + data[i-1].estoque + '<br />';
+		result+='<table border="1"><tr><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr>';
+		result+='<tr><td>' + data[i-1].nome + '</td>' ;
+		result+='<td> R$' + data[i-1].valor + '</td>';
+		result+='<td>' + data[i-1].status + '</td>';
+		result+='<td>' + data[i-1].estoque + '</td></tr>';
 		$('#resultado').html(result);
 	});
 };
