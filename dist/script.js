5d2b5c73638d0d35a8ebfa2824e7821a091f7183
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-05-06 14:48:49 -0300

    Exibir todos produtos

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	$.getJSON('http://192.168.1.109:8080/list', function(data){
-		var list='';
 		var list='<option value="#"> Selecione uma opção. </option>';
 		for (var x=0; x<data.length;x++){
 			list+='<option value='+data[x].chave+'>' +data[x].nome+ '</option>';
 		}
+		list+='<option value=-1> exibir todos produtos </option>';
 		$('#produtos').html(list);
 	});
 });
@@ -23,6 +23,22 @@ function buscarproduto(){
 		});
 	}
 	else {
+		if (i=="#"){
 			$('#resultado').html('');
+		}
+		else if (i<0){
+			$.getJSON('http://192.168.1.109:8080/list', function(data){
+				var result='';
+				result+='<table border="1"><tr><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr><tr>';
+				for (n=0; n<data.length; n++){
+					result+='<tr><td>' + data[n].nome + '</td>' ;
+					result+='<td> R$' + data[n].valor + '</td>';
+					result+='<td>' + data[n].status + '</td>';
+					result+='<td>' + data[n].estoque + '</td></tr>';
+				}
+				'</table>';
+				$('#resultado').html(result);
+			});
+		}
 	}
 };
