bdeb3b7c2b34a4611ea35128cf80ba39268f44d5
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-05-09 16:55:37 -0300

    Layout

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,15 @@
 var endereco='http://192.168.1.109:8080';
 
-function limpar(i){
-	if (i=="#"){
-		$('#resultado').html('');
-	}
-}
-
-function todosprodutos (i){
-	if (i=="@"){
-			$.getJSON(endereco + '/list', function(data){
-				var result='';
-				result+='<table border="1"><tr><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr><tr>';
-				for (n=0; n<data.length; n++){
-					result+='<tr><td>' + data[n].nome + '</td>' ;
-					result+='<td> R$' + data[n].valor + '</td>';
-					result+='<td>' + data[n].status + '</td>';
-					result+='<td>' + data[n].estoque + '</td></tr>';
-				}
-				'</table>';
-				$('#resultado').html(result);
-			});
+$(document).ready(function(){
+	$.getJSON(endereco + '/list', function(data){
+		var list='<option value="#"> Selecione uma opção. </option>';
+		for (var x=0; x<data.length;x++){
+			list+='<option value='+data[x].chave+'>' + data[x].nome + '</option>';
 		}
-}
+		list+='<option value="@"> exibir todos produtos </option>';
+		$('#produtos').html(list);
+	});
+});
 
 function buscarproduto(){
 	var i=$('#produtos').val();
@@ -37,22 +25,31 @@ function buscarproduto(){
 		});
 	}
 
-	
-
 	else {
 		limpar(i);
 		todosprodutos(i);
 	}
-};
+}
 
+function limpar(i){
+	if (i=="#"){
+		$('#resultado').html('');
+	}
+}
 
-$(document).ready(function(){
-	$.getJSON(endereco + '/list', function(data){
-		var list='<option value="#"> Selecione uma opção. </option>';
-		for (var x=0; x<data.length;x++){
-			list+='<option value='+data[x].chave+'>' + data[x].nome + '</option>';
-		}
-		list+='<option value="@"> exibir todos produtos </option>';
-		$('#produtos').html(list);
-	});
-});
+function todosprodutos (i){
+	if (i=="@"){
+		$.getJSON(endereco + '/list', function(data){
+			var result='';
+			result+='<table border="1"><tr><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr><tr>';
+			for (var n=0; n<data.length; n++){
+				result+='<tr><td>' + data[n].nome + '</td>' ;
+				result+='<td> R$' + data[n].valor + '</td>';
+				result+='<td>' + data[n].status + '</td>';
+				result+='<td>' + data[n].estoque + '</td></tr>';
+			}
+			'</table>';
+			$('#resultado').html(result);
+		});
+	}
+}
