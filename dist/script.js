1b8335c5d89a55d1cdb00205f57dd19583d5f266
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-05-09 14:31:42 -0300

    pastas

---
+++ dist/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	$.getJSON('http://192.168.1.109:8080/list', function(data){
+		var list='<option value="#"> Selecione uma opção. </option>';
+		for (var x=0; x<data.length;x++){
+			list+='<option value='+data[x].chave+'>' + data[x].nome + '</option>';
+		}
+		list+='<option value="@"> exibir todos produtos </option>';
+		$('#produtos').html(list);
+	});
+});
+
+function buscarproduto(){
+	var i=$('#produtos').val();
+	if (i>=0){
+		$.getJSON('http://192.168.1.109:8080/product?chave='+i, function(data){
+			var result='';
+			result+='<table border="1"><tr><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr>';
+			result+='<tr><td>' + data.nome + '</td>' ;
+			result+='<td> R$' + data.valor + '</td>';
+			result+='<td>' + data.status + '</td>';
+			result+='<td>' + data.estoque + '</td></tr></table>';
+			$('#resultado').html(result);
+		});
+	}
+	else {
+		if (i=="#"){
+			$('#resultado').html('');
+		}
+		else if (i=="@"){
+			$.getJSON('http://192.168.1.109:8080/list', function(data){
+				var result='';
+				result+='<table border="1"><tr><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr><tr>';
+				for (n=0; n<data.length; n++){
+					result+='<tr><td>' + data[n].nome + '</td>' ;
+					result+='<td> R$' + data[n].valor + '</td>';
+					result+='<td>' + data[n].status + '</td>';
+					result+='<td>' + data[n].estoque + '</td></tr>';
+				}
+				'</table>';
+				$('#resultado').html(result);
+			});
+		}
+	}
+};
